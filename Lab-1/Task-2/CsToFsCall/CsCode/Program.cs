5f025d6406037e0df11a19e889708fea8e6047cd
Bibletoon <alex8.ivanov@gmail.com>
2022-02-11 20:40:52 +0300

    Feat: add cs/fs task 2

---
+++ Lab-1/Task-2/CsToFsCall/CsCode/Program.cs
@@ -0,0 +1,5 @@
+﻿var dto = new FsProgram.UserDto("bibletoon", "Bible Toon", FsProgram.Country.Canada);
+
+var user = FsProgram.createUserFromDto(dto);
+
+Console.WriteLine(user.IsOk ? user.ResultValue.Username : user.ErrorValue.First());
