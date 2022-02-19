ae33f6c044268bc189ab464ffd076547de54fd82
Bibletoon <alex8.ivanov@gmail.com>
2022-02-19 11:59:42 +0300

    Feat: add graph algorithm usage at .net platform

---
+++ Lab-1/Task-3/GraphAlgorithms/GraphAlgorithms/Node.cs
@@ -0,0 +1,26 @@
+﻿namespace GraphAlgorithms;
+
+public class Node<T>
+{
+    private T _value;
+    private readonly HashSet<Node<T>> _neighbors;
+    
+    public Node(T value) {
+        _value = value;
+        _neighbors = new HashSet<Node<T>>();
+    }
+
+    public void connect(Node<T> node)
+    {
+        _neighbors.Add(node);
+        node._neighbors.Add(this);
+    }
+
+    public T getValue() {
+        return _value;
+    }
+
+    public IReadOnlySet<Node<T>> getNeighbors() {
+        return _neighbors;
+    }
+}
