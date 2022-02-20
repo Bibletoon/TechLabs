92b01080543665f7c63a1c4f1f5e5908cdfaa31d
Bibletoon <alex8.ivanov@gmail.com>
2022-02-20 10:24:43 +0300

    Feat: add cs sort benchmark

---
+++ Lab-1/Task-4/CsSortBenchmark/CsSortBenchmark/Benchmark.cs
@@ -0,0 +1,35 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace CsSortBenchmark;
+
+public class Benchmark
+{
+    private int[] _arrayForDefaultSort;
+    private int[] _arrayForMergeSort;
+    
+    [IterationSetup]
+    public void Setup()
+    {
+        _arrayForDefaultSort = new int[2000000];
+        _arrayForMergeSort = new int[2000000];
+        Random r = new Random();
+
+        for (int i = 0; i < 2000000; i++)
+        {
+            _arrayForDefaultSort[i] = r.Next();
+            _arrayForMergeSort[i] = r.Next();
+        }
+    }
+
+    [Benchmark]
+    public void DefaultSort()
+    {
+        Array.Sort(_arrayForDefaultSort);
+    }
+
+    [Benchmark]
+    public void MergeSort()
+    {
+        _arrayForMergeSort = MergeSortAlgorithm.MergeSort(_arrayForMergeSort);
+    }
+}
---
+++ Lab-1/Task-4/CsSortBenchmark/CsSortBenchmark/Program.cs
@@ -0,0 +1,14 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+
+namespace CsSortBenchmark;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var summary = BenchmarkRunner.Run<Benchmark>();
+        Console.WriteLine(summary);
+    }
+}
